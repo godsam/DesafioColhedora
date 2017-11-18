efb0ff9e5b0ad25087c210366fdc49b270593eed
Samuel <samgm8@gmail.com>
2017-11-18 17:45:11 -0200

    teste
    teste

---
+++ Assets/Trator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trator : MonoBehaviour {
+
+	public float velocidade = 5;
+	Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		rb.MovePosition (transform.position + transform.forward * velocidade * Time.deltaTime); 
+	}
+}
