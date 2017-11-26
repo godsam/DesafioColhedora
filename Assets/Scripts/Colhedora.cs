97efd8d96e4958683ed21504160f57d458dc6a8c
Samuel <samgm8@gmail.com>
2017-11-26 21:14:52 -0200

    Distâncias
    Reconhecendo campo e distancia V1

---
+++ Assets/Scripts/Colhedora.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colhedora : MonoBehaviour {
+
+	public terreno terreno;
+
+	bool areaDeColeta;
+	float distancia = 0;
+	int nI = 0;
+
+	Vector3 pA,pB,pC,pD;
+	// Use this for initialization
+	void Start () {
+		pA = terreno.pA;
+		pB = terreno.pB;
+		pC = terreno.pC;
+		pD = terreno.pD;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		distancia = distancia + transform.forward.x * Time.deltaTime;
+
+		areaDeColeta = (pA.x < transform.position.x && pA.z > transform.position.z);
+		areaDeColeta = (areaDeColeta && pB.x > transform.position.x && pB.z > transform.position.z);
+		areaDeColeta = (areaDeColeta && pC.x < transform.position.x && pC.z < transform.position.z);
+		areaDeColeta = (areaDeColeta && pD.x > transform.position.x && pD.z < transform.position.z);
+
+		if (distancia > 1) {
+			distancia = 0;
+		}
+
+		if (areaDeColeta && distancia == 0) {
+			nI++;
+			print ("entrou na area e contou a distancia " + nI);
+		}
+	}
+}
--- Assets/Scripts/terreno.cs
+++ Assets/Scripts/terreno.cs
@@ -34,6 +34,30 @@ public class terreno : MonoBehaviour {
 		Instantiate (ponto1, pC,transform.rotation);
 
 		Instantiate (ponto1, pD,transform.rotation);
+
+		Instantiate (ponto2, pA1,transform.rotation);
+
+		Instantiate (ponto2, pB1,transform.rotation);
+
+		Instantiate (ponto2, pC1,transform.rotation);
+
+		Instantiate (ponto2, pD1,transform.rotation);
+
+		Instantiate (ponto3, pA2,transform.rotation);
+
+		Instantiate (ponto3, pB2,transform.rotation);
+
+		Instantiate (ponto3, pC2,transform.rotation);
+
+		Instantiate (ponto3, pD2,transform.rotation);
+
+		Instantiate (ponto4, pA3,transform.rotation);
+
+		Instantiate (ponto4, pB3,transform.rotation);
+
+		Instantiate (ponto4, pC3,transform.rotation);
+
+		Instantiate (ponto4, pD3,transform.rotation);
 	}
 	
 	// Update is called once per frame
