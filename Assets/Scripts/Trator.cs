0a9a0d7f3ae411e4c432045c4df49911511e8aec
Samuel <samgm8@gmail.com>
2017-11-30 23:33:05 -0200

    andando por pontos
    trator fazendo um percurso
    ROTATEFAIL

--- Assets/Scripts/Trator.cs
+++ Assets/Scripts/Trator.cs
@@ -5,14 +5,32 @@ using UnityEngine;
 public class Trator : MonoBehaviour {
 
 	public float velocidade = 5;
+	Vector3 nextPos;
+	Vector3 direcao;
+	float distancia = 0;
 	Rigidbody rb;
+
+	GameObject[] targets;
+	int i = 0;
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
+		targets = GameObject.FindGameObjectsWithTag ("Pega");
+		nextPos = targets [i].transform.position;
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate () {
-		rb.MovePosition (transform.position + transform.forward * velocidade * Time.deltaTime); 
+	void Update () {
+		distancia = Vector3.Distance (transform.position, nextPos);
+		if (distancia < 0.9f) {
+			i++;
+			if (i >= targets.Length) {
+				i = 0;
+			}
+			nextPos = targets [i].transform.position;
+		}
+
+		direcao = (nextPos - transform.position).normalized;
+		rb.MovePosition (transform.position + direcao * velocidade * Time.deltaTime); 
 	}
 }
